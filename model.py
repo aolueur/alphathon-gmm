242712384fca1b669bbcd44ad141d818ed416858
yuao <yp439@cornell.edu>
2024-09-13 14:59:40 -0400

    feat(gmm): add support for automatically choosing the best number of components based on BIC

--- model.py
+++ model.py
@@ -12,26 +12,51 @@ from numpy.typing import ArrayLike
 
 
 class GaussianMixtureModel:
-    """
-    A Gaussian Mixture Model for clustering factor returns.
+    """A Gaussian Mixture Model for clustering factor returns.
+
+    Attributes:
+        X: array-like of shape (n_samples, n_features).
+            The input data. Each row is a single sample.
+        n_components: int.
+            The number of components (Gaussian distributions) in the mixture.
+            The optimal number of components is determined by the BIC score.
+        model: GaussianMixture.
+            The fitted Gaussian Mixture model
     """
 
-    def __init__(self, n_components: int, X: ArrayLike):
+    def __init__(self, X: ArrayLike):
         """Initialize the GMM model
 
         Args:
             n_components: int.
                 The number of components (Gaussian distributions) in the mixture.
-            X: array-like of shape (n_samples, n_features). 
+            X: array-like of shape (n_samples, n_features).
                 The input data. Each row is a single sample.
         """
-        self.n_components = n_components
         self.X = X
+
+        # Find the optimal number of components
+        n_components_candidates = np.arange(1, 21)
+        models = [GaussianMixture(n, covariance_type='full', random_state=0).fit(X)
+                  for n in n_components_candidates]
+        bics = [model.bic(X) for model in models]  # BIC for each model
+        self.n_components = n_components_candidates[np.argmin(bics)]
+
+        # Fit the model with the optimal number of components
         self.model = GaussianMixture(
-            n_components,
+            self.n_components,
             random_state=0,
             covariance_type='full'
         ).fit(X)
+
+    def n_components(self) -> int:
+        """Get the number of components in the mixture
+
+        Returns:
+            n_components: int.
+                The number of components in the mixture.
+        """
+        return self.n_components
 
     def predict(self, X: ArrayLike) -> np.ndarray:
         """Predict the cluster for each sample in X
@@ -91,25 +116,26 @@ class GaussianMixtureModel:
         """
         return self.model.bic(self.X)
 
-    def plot_aic_bic(self, X: ArrayLike):
-        """Plot AIC and BIC scores for different numbers of components in a GMM.
 
-        Args:
-        X: array-like of shape (n_samples, n_features). 
-            The input data. Each row is a single sample.
-        """
+def plot_aic_bic(X: ArrayLike):
+    """Helper function for plotting the AIC and BIC scores for different number of components
 
-        n_components = np.arange(1, 21)
-        models = [GaussianMixture(n, covariance_type='full', random_state=0).fit(
-            X) for n in n_components]
+    Args:
+    X: array-like of shape (n_samples, n_features). 
+        The input data. Each row is a single sample.
+    """
 
-        plt.plot(n_components, [m.bic(X) for m in models], label='BIC')
-        plt.plot(n_components, [m.aic(X) for m in models], label='AIC')
-        plt.legend(loc='best')
-        plt.xlabel('Number of Components')
-        plt.ylabel('Information Criteria')
-        plt.title('AIC and BIC Scores for Gaussian Mixture Models')
-        plt.show()
+    n_components = np.arange(1, 21)
+    models = [GaussianMixture(n, covariance_type='full', random_state=0).fit(
+        X) for n in n_components]
+
+    plt.plot(n_components, [m.bic(X) for m in models], label='BIC')
+    plt.plot(n_components, [m.aic(X) for m in models], label='AIC')
+    plt.legend(loc='best')
+    plt.xlabel('Number of Components')
+    plt.ylabel('Information Criteria')
+    plt.title('AIC and BIC Scores for Gaussian Mixture Models')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -119,8 +145,8 @@ if __name__ == '__main__':
         period='3mo'
     )['Adj Close'].pct_change().dropna() * 100
 
-    gmm = GaussianMixtureModel(4, data)
+    gmm = GaussianMixtureModel(data)
+    print(gmm.n_components)
     print(gmm.predict(data))
 
-    # Plot AIC and BIC scores to find the optimal number of components
-    gmm.plot_aic_bic(data)
+    plot_aic_bic(data)
