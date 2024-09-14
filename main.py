3d7b05c55a4321625982ffa1da77a48e29515bb8
shshunn <157559257+shshunn@users.noreply.github.com>
2024-09-14 16:31:02 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from model import GaussianMixtureModel
+import utils
 
 # Load the data
 data = pd.read_csv('./clean_data/factor_returns.csv', index_col=0)
@@ -11,4 +12,4 @@ gmm = GaussianMixtureModel(data)
 # Result
 print(gmm.predict(data))
 
-gmm.plot_aic_bic(data)
+plot_aic_bic(data)
